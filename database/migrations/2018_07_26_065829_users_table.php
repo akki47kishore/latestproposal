d74d7ba47aa637df86985d12ecc7073c05f53800
aswinmohan <aswinmohan@qburst.com>
2018-07-27 12:46:44 +0530

    created migration for users_table

---
+++ database/migrations/2018_07_26_065829_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_table',function (Blueprint $table){
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('password');
+            $table->integer('user_status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users_table');
+    }
+}
